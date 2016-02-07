607ff3872270948ddb353dd10d54e54d0fe2a54a
craigtaub <craig.taub@bbc.co.uk>
2016-02-07 12:27:28 +0000

    use app CSS

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const port = 3000;
 
 app.use('/node_modules', Express.static('node_modules'));
 app.use('/app', Express.static('app'));
+app.use('/public', Express.static('public'));
 
 app.get('/', function(req, res) {
   fs.readFile('index.html', function read(err, data) {
